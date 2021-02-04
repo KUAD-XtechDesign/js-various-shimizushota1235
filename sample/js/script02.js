2a5a1885582e5d557f26de20ba1eabf9403854da
shimizushota1235 <68629221+shimizushota1235@users.noreply.github.com>
2021-02-04 15:55:58 +0900

    a

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -50,18 +50,3 @@ $(function(){
 
 
 })
-
-$(function(){
-
-  $(".img_button").on("click",function(){
-    let target = $(this).siblings(".content");
-    target.slideToggle("slow");
-
-    $("html, body").animate({scrollTop:target.offset().top}, "slow");
-  })
-
-  $(".close_button").on("click",function(){
-    $(this).parent(".content").slideUp("slow");
-  })
-
-});
